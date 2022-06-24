c0fc3f7d25dc6c973904a564d7614bd187474907
skybord0 <87891657+skybord0@users.noreply.github.com>
2022-06-24 08:28:45 +0530

    Add files via upload

---
+++ f2.js
@@ -0,0 +1,14 @@
+function validate()
+{
+    var username=document.getElementById("username").Value;
+    var username=document.getElementById("username").Value;
+    if(username=="biswaranjan"&& password=="dash")
+    {
+        alert("login succesfully");
+        return false;
+    }
+    else
+    {
+        alert("Incorrect Identity");
+    }
+}
